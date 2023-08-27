2b153514a12846c507ff4cd8c029d0177631cae8
Shantona <142700469+sfshantona@users.noreply.github.com>
2023-08-27 23:54:31 +0600

    Add files via upload

---
+++ powerful_crift_migelo1.ino
@@ -0,0 +1,24 @@
+//variable Types:
+//Integer 1,2,3,4,.....
+//Float 1.2,3.4,.....
+//Char 'a','r',......
+//String "Hello world","Me",.....
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ //Variable Declaration:
+ //data_type variable name;
+ int robot=2;
+ float cse=2.776;
+ char eee='S';
+ String shantona;
+ shantona="Hello World";
+ Serial.println(cse);
+  
+  
+}
