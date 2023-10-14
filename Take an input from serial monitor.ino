424a25ed93713e88087b5b76fa6e5d1ce762e8e8
Shantona <142700469+sfshantona@users.noreply.github.com>
2023-10-15 03:01:49 +0600

    Add files via upload

---
+++ Take an input from serial monitor.ino
@@ -0,0 +1,22 @@
+//Take an Input from Serial Monitor
+
+void setup()
+{
+Serial.begin(9600);
+   
+}
+
+void loop()
+{
+//float a=Serial.parseFloat();
+  //int a=Serial.parseInt();
+  
+  //char a=Serial.read();
+  //delay(1000);
+  
+  //String a=Serial.readString();
+  
+  Serial.println(a);
+ 
+  
+}
