76cdf98a4d54a3756233320031eb100e0928cf81
Shantona <142700469+sfshantona@users.noreply.github.com>
2023-10-15 03:01:09 +0600

    Add files via upload

---
+++ feet to inch converter.ino
@@ -0,0 +1,19 @@
+//feet to inch converter program
+
+void setup()
+{
+Serial.begin(9600);
+  float feet=150;
+  float inch;
+  
+  inch=12*feet;
+ 
+ Serial.println(inch); 
+  
+}
+
+void loop()
+{
+ 
+  
+}
