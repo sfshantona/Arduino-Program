8deb241dc6924db5c63ec73421c2defc5eef446d
Shantona <142700469+sfshantona@users.noreply.github.com>
2023-10-15 02:53:45 +0600

    Add files via upload

---
+++ Increment Decrement Operator.ino
@@ -0,0 +1,25 @@
+//Increment/Decrement Operator
+
+void setup()
+{
+Serial.begin(9600);
+  int i;
+  i=5;
+  Serial.println(i);
+  //i=i+1;
+  //i++;
+  //i++;
+  i--;
+  i--;
+  Serial.println(i);
+ 
+ 
+ 
+  
+}
+
+void loop()
+{
+ 
+  
+}
